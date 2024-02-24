369540f47259420f63e677f8426aceffa5823dfe
David Rotermund <54365609+davrot@users.noreply.github.com>
2024-02-24 17:28:26 +0100

    Add files via upload

---
+++ new_pipeline/stage_1_get_ref_image.py
@@ -0,0 +1,143 @@
+import json
+import os
+from jsmin import jsmin  # type: ignore
+import torch
+import numpy as np
+
+
+from functions.get_experiments import get_experiments
+from functions.get_trials import get_trials
+from functions.get_parts import get_parts
+from functions.bandpass import bandpass
+from functions.create_logger import create_logger
+from functions.load_meta_data import load_meta_data
+
+mylogger = create_logger(
+    save_logging_messages=True, display_logging_messages=True, log_stage_name="stage_1"
+)
+
+mylogger.info("loading config file")
+with open("config.json", "r") as file:
+    config = json.loads(jsmin(file.read()))
+
+if torch.cuda.is_available():
+    device_name: str = "cuda:0"
+else:
+    device_name = "cpu"
+
+if config["force_to_cpu"]:
+    device_name = "cpu"
+
+mylogger.info(f"Using device: {device_name}")
+device: torch.device = torch.device(device_name)
+
+dtype_str: str = config["dtype"]
+dtype: torch.dtype = getattr(torch, dtype_str)
+
+raw_data_path: str = os.path.join(
+    config["basic_path"],
+    config["recoding_data"],
+    config["mouse_identifier"],
+    config["raw_path"],
+)
+
+mylogger.info(f"Using data path: {raw_data_path}")
+
+first_experiment_id: int = int(get_experiments(raw_data_path).min())
+first_trial_id: int = int(get_trials(raw_data_path, first_experiment_id).min())
+first_part_id: int = int(
+    get_parts(raw_data_path, first_experiment_id, first_trial_id).min()
+)
+
+filename_data: str = os.path.join(
+    raw_data_path,
+    f"Exp{first_experiment_id:03d}_Trial{first_trial_id:03d}_Part{first_part_id:03d}.npy",
+)
+
+mylogger.info(f"Will use: {filename_data} for data")
+
+filename_meta: str = os.path.join(
+    raw_data_path,
+    f"Exp{first_experiment_id:03d}_Trial{first_trial_id:03d}_Part{first_part_id:03d}_meta.txt",
+)
+
+mylogger.info(f"Will use: {filename_meta} for meta data")
+
+meta_channels: list[str]
+meta_mouse_markings: str
+meta_recording_date: str
+meta_stimulation_times: dict
+meta_experiment_names: dict
+meta_trial_recording_duration: float
+meta_frame_time: float
+meta_mouse: str
+
+(
+    meta_channels,
+    meta_mouse_markings,
+    meta_recording_date,
+    meta_stimulation_times,
+    meta_experiment_names,
+    meta_trial_recording_duration,
+    meta_frame_time,
+    meta_mouse,
+) = load_meta_data(mylogger=mylogger, filename_meta=filename_meta)
+
+
+dtype_str = config["dtype"]
+dtype_np: np.dtype = getattr(np, dtype_str)
+
+mylogger.info("Loading data")
+data = torch.tensor(
+    np.load(filename_data).astype(dtype_np), dtype=dtype, device=torch.device("cpu")
+)
+mylogger.info("-==- Done -==-")
+
+output_path = config["ref_image_path"]
+mylogger.info(f"Create directory {output_path} in the case it does not exist")
+os.makedirs(output_path, exist_ok=True)
+
+mylogger.info("Reference images")
+for i in range(0, len(meta_channels)):
+    temp_path: str = os.path.join(output_path, meta_channels[i] + ".npy")
+    mylogger.info(f"Extract and save: {temp_path}")
+    frame_id: int = data.shape[-2] // 2
+    mylogger.info(f"Will use frame id: {frame_id}")
+    ref_image: np.ndarray = (
+        data[:, :, frame_id, meta_channels.index(meta_channels[i])]
+        .clone()
+        .cpu()
+        .numpy()
+    )
+    np.save(temp_path, ref_image)
+mylogger.info("-==- Done -==-")
+
+sample_frequency: float = 1.0 / meta_frame_time
+mylogger.info(
+    (
+        f"Heartbeat power {config['lower_freqency_bandpass']}Hz"
+        f" - {config['upper_freqency_bandpass']}Hz,"
+        f" sample-rate: {sample_frequency},"
+        f" skipping the first {config['skip_frames_in_the_beginning']} frames"
+    )
+)
+
+for i in range(0, len(meta_channels)):
+    temp_path = os.path.join(output_path, meta_channels[i] + "_var.npy")
+    mylogger.info(f"Extract and save: {temp_path}")
+
+    heartbeat_ts: torch.Tensor = bandpass(
+        data=data[..., i],
+        device=data.device,
+        low_frequency=config["lower_freqency_bandpass"],
+        high_frequency=config["upper_freqency_bandpass"],
+        fs=sample_frequency,
+        filtfilt_chuck_size=10,
+    )
+
+    heartbeat_power = heartbeat_ts[..., config["skip_frames_in_the_beginning"] :].var(
+        dim=-1
+    )
+    np.save(temp_path, heartbeat_power)
+
+mylogger.info("-==- Done -==-")
---
+++ new_pipeline/stage_2_make_heartbeat_mask.py
@@ -0,0 +1,155 @@
+import matplotlib.pyplot as plt  # type:ignore
+import matplotlib
+import numpy as np
+import torch
+import os
+import json
+
+from jsmin import jsmin  # type:ignore
+from matplotlib.widgets import Slider, Button  # type:ignore
+from functools import partial
+from functions.gauss_smear_individual import gauss_smear_individual
+from functions.create_logger import create_logger
+
+
+mylogger = create_logger(
+    save_logging_messages=True, display_logging_messages=True, log_stage_name="stage_2"
+)
+
+mylogger.info("loading config file")
+with open("config.json", "r") as file:
+    config = json.loads(jsmin(file.read()))
+
+threshold: float = 0.05
+path: str = config["ref_image_path"]
+
+image_ref_file: str = os.path.join(path, "donor.npy")
+image_var_file: str = os.path.join(path, "donor_var.npy")
+heartbeat_mask_file: str = os.path.join(path, "heartbeat_mask.npy")
+heartbeat_mask_threshold_file: str = os.path.join(path, "heartbeat_mask_threshold.npy")
+
+if torch.cuda.is_available():
+    device_name: str = "cuda:0"
+else:
+    device_name = "cpu"
+
+if config["force_to_cpu"]:
+    device_name = "cpu"
+
+mylogger.info(f"Using device: {device_name}")
+device: torch.device = torch.device(device_name)
+
+
+def next_frame(
+    i: float, images: np.ndarray, image_handle: matplotlib.image.AxesImage
+) -> None:
+    global threshold
+    threshold = i
+
+    display_image: np.ndarray = images.copy()
+    display_image[..., 2] = display_image[..., 0]
+    mask: np.ndarray = np.where(images[..., 2] >= i, 1.0, np.nan)[..., np.newaxis]
+    display_image *= mask
+    display_image = np.nan_to_num(display_image, nan=1.0)
+
+    image_handle.set_data(display_image)
+    return
+
+
+def on_clicked_accept(event: matplotlib.backend_bases.MouseEvent) -> None:
+    global threshold
+    global volume_3color
+    global path
+    global mylogger
+    global heartbeat_mask_file
+    global heartbeat_mask_threshold_file
+
+    mylogger.info(f"Threshold: {threshold}")
+
+    mask: np.ndarray = volume_3color[..., 2] >= threshold
+    mylogger.info(f"Save mask to: {heartbeat_mask_file}")
+    np.save(heartbeat_mask_file, mask)
+    mylogger.info(f"Save threshold to: {heartbeat_mask_threshold_file}")
+    np.save(heartbeat_mask_threshold_file, np.array([threshold]))
+    exit()
+
+
+def on_clicked_cancel(event: matplotlib.backend_bases.MouseEvent) -> None:
+    exit()
+
+
+mylogger.info(f"loading image reference file: {image_ref_file}")
+image_ref: np.ndarray = np.load(image_ref_file)
+image_ref /= image_ref.max()
+
+mylogger.info(f"loading image heartbeat power: {image_var_file}")
+image_var: np.ndarray = np.load(image_var_file)
+image_var /= image_var.max()
+
+mylogger.info("Smear the image heartbeat power patially")
+temp, _ = gauss_smear_individual(
+    input=torch.tensor(image_var[..., np.newaxis], device=device),
+    spatial_width=4.0,
+    temporal_width=0.1,
+    use_matlab_mask=False,
+)
+temp /= temp.max()
+
+mylogger.info("-==- DONE -==-")
+
+volume_3color = np.concatenate(
+    (
+        np.zeros_like(image_ref[..., np.newaxis]),
+        image_ref[..., np.newaxis],
+        temp.cpu().numpy(),
+    ),
+    axis=-1,
+)
+
+mylogger.info("Prepare image")
+
+display_image = volume_3color.copy()
+display_image[..., 2] = display_image[..., 0]
+mask = np.where(volume_3color[..., 2] >= threshold, 1.0, np.nan)[..., np.newaxis]
+display_image *= mask
+display_image = np.nan_to_num(display_image, nan=1.0)
+
+value_sort = np.sort(image_var.flatten())
+value_sort_max = value_sort[int(value_sort.shape[0] * 0.95)]
+mylogger.info("-==- DONE -==-")
+
+mylogger.info("Create figure")
+
+fig: matplotlib.figure.Figure = plt.figure()
+
+image_handle = plt.imshow(display_image, vmin=0, vmax=1, cmap="hot")
+
+mylogger.info("Add controls")
+
+axfreq = fig.add_axes(rect=(0.4, 0.9, 0.3, 0.03))
+slice_slider = Slider(
+    ax=axfreq,
+    label="Slice",
+    valmin=0,
+    valmax=value_sort_max,
+    valinit=threshold,
+    valstep=value_sort_max / 100.0,
+)
+axbutton_accept = fig.add_axes(rect=(0.3, 0.85, 0.2, 0.04))
+button_accept = Button(
+    ax=axbutton_accept, label="Accept", image=None, color="0.85", hovercolor="0.95"
+)
+button_accept.on_clicked(on_clicked_accept)  # type: ignore
+
+axbutton_cancel = fig.add_axes(rect=(0.55, 0.85, 0.2, 0.04))
+button_cancel = Button(
+    ax=axbutton_cancel, label="Cancel", image=None, color="0.85", hovercolor="0.95"
+)
+button_cancel.on_clicked(on_clicked_cancel)  # type: ignore
+
+slice_slider.on_changed(
+    partial(next_frame, images=volume_3color, image_handle=image_handle)
+)
+
+mylogger.info("Display")
+plt.show()
