861fd3162080912c96c5848cd8ebecc45275a637
David Rotermund <54365609+davrot@users.noreply.github.com>
2024-02-28 15:15:37 +0100

    Add files via upload

--- new_pipeline/functions/load_meta_data.py
+++ new_pipeline/functions/load_meta_data.py
@@ -3,44 +3,53 @@ import json
 
 
 def load_meta_data(
-    mylogger: logging.Logger, filename_meta: str
+    mylogger: logging.Logger, filename_meta: str, silent_mode=False
 ) -> tuple[list[str], str, str, dict, dict, float, float, str]:
 
-    mylogger.info("Loading meta data")
+    if silent_mode is False:
+        mylogger.info("Loading meta data")
     with open(filename_meta, "r") as file_handle:
         metadata: dict = json.load(file_handle)
 
     channels: list[str] = metadata["channelKey"]
 
-    mylogger.info(f"meta data: channel order: {channels}")
+    if silent_mode is False:
+        mylogger.info(f"meta data: channel order: {channels}")
 
     mouse_markings: str = metadata["sessionMetaData"]["mouseMarkings"]
-    mylogger.info(f"meta data: mouse markings: {mouse_markings}")
+    if silent_mode is False:
+        mylogger.info(f"meta data: mouse markings: {mouse_markings}")
 
     recording_date: str = metadata["sessionMetaData"]["date"]
-    mylogger.info(f"meta data: recording data: {recording_date}")
+    if silent_mode is False:
+        mylogger.info(f"meta data: recording data: {recording_date}")
 
     stimulation_times: dict = metadata["sessionMetaData"]["stimulationTimes"]
-    mylogger.info(f"meta data: stimulation times: {stimulation_times}")
+    if silent_mode is False:
+        mylogger.info(f"meta data: stimulation times: {stimulation_times}")
 
     experiment_names: dict = metadata["sessionMetaData"]["experimentNames"]
-    mylogger.info(f"meta data: experiment names: {experiment_names}")
+    if silent_mode is False:
+        mylogger.info(f"meta data: experiment names: {experiment_names}")
 
     trial_recording_duration: float = float(
         metadata["sessionMetaData"]["trialRecordingDuration"]
     )
-    mylogger.info(
-        f"meta data: trial recording duration: {trial_recording_duration} sec"
-    )
+    if silent_mode is False:
+        mylogger.info(
+            f"meta data: trial recording duration: {trial_recording_duration} sec"
+        )
 
     frame_time: float = float(metadata["sessionMetaData"]["frameTime"])
-    mylogger.info(
-        f"meta data: frame time: {frame_time} sec ; frame rate: {1.0/frame_time}Hz"
-    )
+    if silent_mode is False:
+        mylogger.info(
+            f"meta data: frame time: {frame_time} sec ; frame rate: {1.0/frame_time}Hz"
+        )
 
     mouse: str = metadata["sessionMetaData"]["mouse"]
-    mylogger.info(f"meta data: mouse: {mouse}")
-    mylogger.info("-==- Done -==-")
+    if silent_mode is False:
+        mylogger.info(f"meta data: mouse: {mouse}")
+        mylogger.info("-==- Done -==-")
 
     return (
         channels,
